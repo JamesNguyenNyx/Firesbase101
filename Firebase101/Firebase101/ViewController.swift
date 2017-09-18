7b4fba05808d1b9429c42012004d15d8a1b4797f
Nguyen An Tinh <jamesnguyen.nyx@gmail.com>
2017-09-18 15:09:42 +0700

    Initial project

---
+++ Firebase101/Firebase101/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Firebase101
+//
+//  Created by James Nguyen on 2017/09/14.
+//  Copyright © 2017 James Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
